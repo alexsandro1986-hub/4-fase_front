e89bc02d6fb939ed8827e4ebded05e63930568c0
ALEXSANDRO SILVA SILVA <alexsandro_silva16@estudante.sesisenai.org.br>
2024-08-01 21:47:50 -0300

    1 front

---
+++ aula00/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Conteudo from './components/Conteudo';
+import Header from './components/Header';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hello</h1>
+     <Header/>
+      <Conteudo/> 
+    </>
+  )
+}
+
+export default App
---
+++ aula00/src/components/Conteudo/index.jsx
@@ -0,0 +1,14 @@
+
+
+function Conteudo() {
+    return (
+        <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Atque ad unde id totam illum esse praesentium, ducimus nihil
+                distinctio reiciendis deserunt repudiandae.
+                Est id quisquam nihil aut praesentium consectetur doloribus!</p>
+        </div>
+    )
+}
+
+export default Conteudo
